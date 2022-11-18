af84b1752fce371b0af125ed10da0d1a0f071fd6
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-11-19 02:15:29 +0300

    add google api and googleMapReact

---
+++ src/components/Map.js
@@ -0,0 +1,23 @@
+import GoogleMapReact from "google-map-react";
+
+const Map = ({ center, zoom }) => {
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCEC61-3Jfou2DOH3quO4tRTVnNVuwEiC4" }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 42.3265,
+    lng: -122.8756,
+  },
+  zoom: 6,
+};
+
+export default Map;
